3729b854924efc880dd776ae6af8ce9ab4d2649a
Pai Khant Ko <kopaikhant@gmail.com>
2024-12-15 22:12:31 +0000

    Backend Commit : Add Appointment and Disease API Endpoints

--- backend/vaccination_tracking_app/serializers.py
+++ backend/vaccination_tracking_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser, AvailableTimeSlot
+from .models import CustomUser, AvailableTimeSlot, Disease, Appointment
 
 class PatientRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -20,8 +20,48 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
         )
         return user
 
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'gender']
+
 class AvailableTimeSlotsSerializer(serializers.ModelSerializer):
+    doctor = DoctorSerializer(read_only=True)
 
     class Meta:
         model = AvailableTimeSlot
-        fields = '__all__'
+        fields = ['id', 'available_datetime', 'is_available', 'doctor']
+
+class DiseaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disease
+        fields = ['id', 'disease_name', 'disease_severity']
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'date_of_birth', 'gender', 'address']
+
+class BookAppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = ['consultation', 'patient', 'disease']
+
+class AppointmentsSerializer(serializers.ModelSerializer):
+    consultation = AvailableTimeSlotsSerializer(read_only=True)
+    patient = PatientSerializer(read_only=True)
+    disease = DiseaseSerializer(read_only=True)
+
+    class Meta:
+        model = Appointment
+        fields = ['id', 'consultation', 'patient', 'disease']
+
+        def create(self, validated_data):
+            # Create the appointment
+            appointment = super().create(validated_data)
+
+            # Update the doctor's availability
+            consultation = appointment.consultation
+            consultation.is_available = False  # Set the flag to False
+            consultation.save()
+            return appointment
--- backend/vaccination_tracking_app/urls.py
+++ backend/vaccination_tracking_app/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from .views.patient_registration_view import PatientRegistrationView
 from .views.login_view import LoginView
 from .views.available_time_slots_view import AvailableTimeSlotsView
+from .views.diseases_view import DiseasesView
+from .views.appointments_view import AppointmentsPerPatientView
 
 
 urlpatterns = [
     path('register/', PatientRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('timeslots/', AvailableTimeSlotsView.as_view() , name='timeslots')
-    
+    path('timeslots/', AvailableTimeSlotsView.as_view() , name='timeslots'),
+    path('diseases/', DiseasesView.as_view(), name='diseases'),
+    path('appointments/', AppointmentsPerPatientView.as_view(), name='appointments')
 ]
